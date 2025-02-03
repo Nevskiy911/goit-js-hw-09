0764b831d7044c9d44149e3a8f7bdeeaa06fc41c
Nevskiy911 <maleev1820@gmail.com>
2025-02-03 21:45:55 +0100

    fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const galleryLink = document.querySelector('a[href="./1-gallery.html"]');
-  const formLink = document.querySelector('a[href="./2-form.html"]');
+  const galleryLink = document.querySelector('a[href="/1-gallery.html"]');
+  const formLink = document.querySelector('a[href="/2-form.html"]');
   
   galleryLink.addEventListener('click', (e) => {
     console.log("Redirecting to Gallery");
